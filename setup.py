1823d69fa4cf6b6c0adb89dd465719697a0c5df5
FelipeSchreiber <schreiber.felipe@poli.ufrj.br>
2023-03-26 12:46:55 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='bregClust',
+version='0.1',
+description='An example package',
+url='#',
+author='max',
+install_requires=[''],
+author_email='',
+packages=setuptools.find_packages(),
+zip_safe=False)
