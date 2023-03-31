e4d7143c34ede0e61eb0ea1e23d2463e0f4b32d0
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-03-31 11:28:58 -0300

    ""

--- BregmanClusteringTorch/torch_models.py
+++ BregmanClusteringTorch/torch_models.py
@@ -9,7 +9,7 @@ felipesc@cos.ufrj.br
 
 import scipy as sp
 from sklearn.base import BaseEstimator, ClusterMixin
-from BREGMANCLUSTERING.BregmanClustering import *
+from BregmanClustering.BregmanClustering import *
 from .torch_divergences import *
 import torch
 device = "cuda" if torch.cuda.is_available() else "cpu"
