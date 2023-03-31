627a30c89fb866500550c8504b83273b61a1d479
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-03-31 12:25:16 -0300

    ""

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ version='0.1',
 description='A package for clustering attributed networks',
 url='#',
 author='Felipe Schreiber Fernandes',
-install_requires=["torch_geometric","torch","sklearn>=1.2.2","numpy>=1.22.4","scipy>=1.10.1"],
+install_requires=["torch_geometric","torch","scikit-learn>=1.2.2","numpy>=1.22.4","scipy>=1.10.1"],
 author_email='felipesc@cos.ufrj.br',
 packages=setuptools.find_packages(exclude=["tests"]),
 zip_safe=False)
