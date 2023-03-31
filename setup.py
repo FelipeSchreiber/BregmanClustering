d55d07adb46d35dc7449a80beab9e68e3bd2bd50
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-03-31 17:27:44 -0300

    "Added torch in dependencies and changed tests"

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ version='0.1',
 description='A package for clustering attributed networks',
 url='#',
 author='Felipe Schreiber Fernandes',
-install_requires=[""],
+install_requires=["torch"],
 author_email='felipesc@cos.ufrj.br',
 packages=setuptools.find_packages(exclude=["tests"]),
 zip_safe=False)
