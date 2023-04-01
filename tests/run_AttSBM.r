b15e2feae162fa02b3998463cf6562e90b92d88b
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-03-31 21:30:25 -0300

    updated tests

--- tests/run_AttSBM.r
+++ tests/run_AttSBM.r
@@ -1,8 +1,7 @@
 #!/usr/bin/env Rscript
 library(reticulate)
-#source('/home/felipe/Documentos/AttributedSBM-master/FitAttribute.R')
+source('../AttributedSBM/FitAttribute.R')
 args = commandArgs(trailingOnly=TRUE)
-source(args[4])
 # Create a file
 file.create("predict.npy")
 np <- import("numpy")
