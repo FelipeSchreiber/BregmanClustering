2d6c36ae1ce2acd397a683925bf061a0018a4832
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-04-01 23:00:20 -0300

    Agora vai

--- BregmanTests/install_algorithms.py
+++ BregmanTests/install_algorithms.py
@@ -27,9 +27,7 @@ def modify_csbm(path):
 
 def main():
     print("Downloading packages from github...\n")
-    #bash_path = os.path.abspath(bregClust.__file__)
-    #bash_path = f"{cfg.install_breg_path}/BregmanTests"
-    bash_path = resource_filename("bregClust-1.0","bin")
+    bash_path = resource_filename("BregmanTests","")
     print(bash_path)
     subprocess.call([f"{bash_path}/install_algos.sh"])
     modify_csbm("./CSBM/Python/functions.py")
