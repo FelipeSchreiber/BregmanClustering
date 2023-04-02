72cb285adb78cc5a6d888ca5860644f1da008fc9
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-04-02 00:07:59 -0300

    This is it

--- BregmanTests/install_algorithms.py
+++ BregmanTests/install_algorithms.py
@@ -29,7 +29,7 @@ def main():
     print("Downloading packages from github...\n")
     bash_path = resource_filename("BregmanTests","")+"/install_algos.sh"
     subprocess.call(["chmod","777",f"{bash_path}"])
-    subprocess.call([f"{bash_path}/install_algos.sh"])
+    subprocess.call([f"{bash_path}"])
     modify_csbm("./CSBM/Python/functions.py")
     modify_att_sbm(path_to_att_sbm)
     print("Installing R packages...\n")
