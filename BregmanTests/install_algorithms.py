d1f02be3f80f42c98d08d7ba237057dbe82cdd34
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-04-01 21:46:56 -0300

    Automated package download from github

---
+++ BregmanTests/install_algorithms.py
@@ -0,0 +1,42 @@
+import subprocess
+import fileinput
+import rpy2
+from rpy2.robjects.vectors import StrVector
+from rpy2.robjects.packages import importr
+import os
+
+path = "./data/Benchmark/"
+path_to_att_sbm = "./AttributedSBM/FitAttribute.R"
+
+def modify_att_sbm(path):
+    for i,line in enumerate(fileinput.input(path, inplace=True)):
+        if i == 0: 
+            print(f'{line.replace(")",",kmeansinit)")}', end='')
+        elif i ==  27:
+            print(f'#{line}', end='')
+        else:
+            print(line)
+
+def modify_csbm(path):
+    for i,line in enumerate(fileinput.input(path, inplace=True)):
+        if "306" in line: 
+            print(f'{line.replace("306","n")}')
+        else:
+            print(line)
+
+def main():
+    print("Downloading packages from github...\n")
+    bash_path = os.path.abspath(bregClust.__file__)
+    print(bash_path)
+    subprocess.call([f"{bash_path}/install_algos.sh"])
+    modify_csbm("./CSBM/Python/functions.py")
+    modify_att_sbm(path_to_att_sbm)
+    print("Installing R packages...\n")
+    utils = importr('utils')
+    utils.chooseCRANmirror(ind=1)
+    # Install packages
+    packnames = ("igraph", "reticulate","mvtnorm")
+    utils.install_packages(StrVector(packnames))
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
-import setuptools
-setuptools.setup(name='bregClust',
+import fnmatch
+from setuptools import find_packages, setup
+from setuptools.command.build_py import build_py as build_py_orig
+
+excluded = ['tests/*.ipynb','tests/*.eps','tests/.npy']
+
+class build_py(build_py_orig):
+    def find_package_modules(self, package, package_dir):
+        modules = super().find_package_modules(package, package_dir)
+        return [
+            (pkg, mod, file)
+            for (pkg, mod, file) in modules
+            if not any(fnmatch.fnmatchcase(file, pat=pattern) for pattern in excluded)
+        ]
+    
+setup(name='bregClust',
 version='0.1',
 description='A package for clustering attributed networks',
 url='#',
-author='Felipe Schreiber Fernandes',
-install_requires=[""],
+author='Felipe Schreiber Fernandes, Maximilien Dreveton',
+install_requires=["torch_geometric","rpy2"],
 author_email='felipesc@cos.ufrj.br',
-packages=setuptools.find_packages(exclude=["tests"]),
+packages=find_packages(),
+cmdclass={'build_py': build_py},
+package_data = {'tests': ['*.r']},
 zip_safe=False)
