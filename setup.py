e543528fdf6d497cb0d29da4fa582d05be5e5ffc
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-04-01 23:27:28 -0300

    Overriding install method

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 import fnmatch
 from setuptools import find_packages, setup
 from setuptools.command.build_py import build_py as build_py_orig
+from setuptools.command.install_scripts import install_scripts
+import os
+from pkg_resources import resource_filename
+
+class OverrideInstall(install):
+
+    def run(self):    
+        bash_path = resource_filename("BregmanTests","")+"/install_algos.sh"
+        mode = 777
+        install.run(self) # calling install.run(self) insures that everything that happened previously still happens, so the installation does not break! 
+        # here we start with doing our overriding and private magic ..
+        
+        print("Changing permissions of %s to %s" %
+                         (bash_path, oct(mode)))
+        os.chmod(bash_path, mode)
 
 excluded = ['tests/*.ipynb','tests/*.eps','tests/.npy']
 
@@ -13,7 +28,7 @@ class build_py(build_py_orig):
             if not any(fnmatch.fnmatchcase(file, pat=pattern) for pattern in excluded)
         ]
     
-s = setup(name='bregClust',
+setup(name='bregClust',
 version='1.0',
 description='A package for clustering attributed networks',
 url='#',
@@ -21,6 +36,6 @@ author='Felipe Schreiber Fernandes, Maximilien Dreveton',
 install_requires=["torch_geometric","rpy2"],
 author_email='felipesc@cos.ufrj.br',
 packages=find_packages(),
-cmdclass={'build_py': build_py},
+cmdclass={'build_py': build_py,"install":OverrideInstall},
 package_data = {'BregmanTests': ['*.r',"*.sh"]},
 zip_safe=False)
