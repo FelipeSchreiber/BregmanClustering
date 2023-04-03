99ff7fe42c41b3c50769dc33ad2ff8c0271ca19a
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-04-03 10:19:22 -0300

    ""

--- BregmanClusteringTorch/torch_models.py
+++ BregmanClusteringTorch/torch_models.py
@@ -472,7 +472,9 @@ class GNNBregmanClustering( BaseEstimator, ClusterMixin ):
         X = nx.adjacency_matrix(G).todense()
         X = torch.tensor(X,dtype=torch.float) ## Network data
         Y = torch.tensor(Y,dtype=torch.float) ## attributes
-        graph_data = from_networkx(G,group_node_attrs=['attr'])
+        #graph_data = from_networkx(G,group_node_attrs=['attr'])
+        edge_index = torch.nonzero(X, dtype=torch.long)
+        graph_data = Data(x=Y, edge_index=edge_index,edge_attr=X[edge_index[:,0],edge_index[:,1]])
         optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
         Z = total = 0
         graph_data.x = Tensor.float(graph_data.x)
