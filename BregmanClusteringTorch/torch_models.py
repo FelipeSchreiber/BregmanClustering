8b5d4db2864296f0607bedbd4650693ca75a6aa7
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-04-30 17:07:30 -0300

    ""

--- BregmanClusteringTorch/torch_models.py
+++ BregmanClusteringTorch/torch_models.py
@@ -234,10 +234,10 @@ class BregmanEdgeClusteringTorch( BaseEstimator, ClusterMixin ):
         return attribute_means
     
     def computeGraphMeans( self, A, Z ):
-        M = (Z.T @ Z).type(dtype).to(device)
-        print(M.dtype,M.device,M.item()) 
-        print(torch.linalg.matrix_rank(M))
-        normalisation = torch.linalg.pinv(M)
+        M = (Z.T @ Z).type(dtype).to(device).pinverse()
+        print(M.dtype,M.device,M) 
+        #print(torch.linalg.matrix_rank(M))
+        #normalisation = torch.linalg.pinv(M)
         return normalisation @ Z.T @ A @ Z @ normalisation
     
     def computeEdgeMeans( self, X, Z ):
