e998c3e770b6ca8891b27e11d49f21261bfa61da
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-04-30 19:19:07 -0300

    Changed bash_path

--- BregmanTests/cfg.py
+++ BregmanTests/cfg.py
@@ -2,7 +2,7 @@ from pkg_resources import resource_filename
 path_to_data = "./data/Benchmark/"
 path_to_att_sbm = "./AttributedSBM/FitAttribute.R"
 base_path = resource_filename("BregmanTests","")
-bash_path = base_path+"/install_algos.sh"
+bash_path = (base_path+"/install_algos.sh")[0]
 CRAN_repo = None
 IGNORE_WARNINGS = True
 other_algos_installed = False
