30628f4a611fcb177a63a75ee22579204492cd31
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-04-29 21:25:49 -0300

    ""

--- BregmanClusteringTorch/torch_models.py
+++ BregmanClusteringTorch/torch_models.py
@@ -291,9 +291,8 @@ class BregmanEdgeClusteringTorch( BaseEstimator, ClusterMixin ):
     
     def assignments( self, A, X, Y ):
         z = torch.zeros( (Y.shape[ 0 ],self.n_clusters)).to(device)
-        #H = self.attribute_divergence( Y, self.attribute_means )
         H = self.reduce_by(
-                    self.attribute_divergence(Y[:,None], self.attribute_means[None,:]),
+                    self.attribute_divergence(Y[:,None], self.attribute_means[None,:]),\
                     dim=-1
         )
         for node in range( z.shape[0] ):
