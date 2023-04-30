23bfaf1f3eac0ff18f4053020c3f71bdcb0bbff5
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-04-30 17:54:46 -0300

    ""

--- BregmanClusteringTorch/torch_models.py
+++ BregmanClusteringTorch/torch_models.py
@@ -119,6 +119,7 @@ class BregmanEdgeClusteringTorch( BaseEstimator, ClusterMixin ):
             X = torch.tensor(X).type(dtype).to(device)
             Y = torch.tensor(Y).type(dtype).to(device)
             print(self.predicted_memberships.device,A.device,X.device,Y.device)
+            print(self.predicted_memberships.dtype,A.dtype,X.dtype,Y.dtype)
             self.edge_index = torch.nonzero(A).to(device)
             self.attribute_means = self.computeAttributeMeans(Y,self.predicted_memberships)
             self.graph_means = self.computeGraphMeans(A,self.predicted_memberships)
