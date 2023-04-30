216b47f5ad09cb38a080fec82f147fc631043600
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-04-30 14:32:06 -0300

    ""

--- BregmanClusteringTorch/torch_models.py
+++ BregmanClusteringTorch/torch_models.py
@@ -116,13 +116,9 @@ class BregmanEdgeClusteringTorch( BaseEstimator, ClusterMixin ):
            
         if platform == "win32":
             self.predicted_memberships.to(device)
-            A = torch.tensor(A).type(dtype)
-            A.to(device)
-            X = torch.tensor(X).type(dtype)
-            X.to(device)
-            Y = torch.tensor(Y).type(dtype)
-            Y.to(device)
-            print(device)
+            A = torch.tensor(A).type(dtype).to(device)
+            X = torch.tensor(X).type(dtype).to(device)
+            Y = torch.tensor(Y).type(dtype).to(device)
             print(self.predicted_memberships.device,A.device,Y.device,X.device)
             self.edge_index = torch.nonzero(A).to("cpu")
             self.attribute_means = self.computeAttributeMeans(Y,self.predicted_memberships)
