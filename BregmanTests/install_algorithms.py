3830de2567277b924507d1cd0259596093f691a6
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-04-30 13:07:35 -0300

    ""

--- BregmanTests/install_algorithms.py
+++ BregmanTests/install_algorithms.py
@@ -34,14 +34,15 @@ def modify_csbm(path):
 def main():
     print("Downloading packages from github...\n")
     #subprocess.call(["chmod","777",f"{bash_path}"])
-    os.chmod(bash_path, 777)
-    if platform == "win32":
-        subprocess.call(["git","clone","https://github.com/glmbraun/CSBM/"])
-        subprocess.call(["git","clone","https://github.com/stanleyn/AttributedSBM.git"])
-    else:
-        subprocess.call([f"{bash_path}"])
-    modify_csbm("./CSBM/Python/functions.py")
-    modify_att_sbm("./AttributedSBM/FitAttribute.R")
+    if not os.path.isdir('CSBM'):
+        os.chmod(bash_path, 777)
+        if platform == "win32":
+            subprocess.call(["git","clone","https://github.com/glmbraun/CSBM/"])
+            subprocess.call(["git","clone","https://github.com/stanleyn/AttributedSBM.git"])
+        else:
+            subprocess.call([f"{bash_path}"])
+        modify_csbm("./CSBM/Python/functions.py")
+        modify_att_sbm("./AttributedSBM/FitAttribute.R")
     print("Installing R packages...\n This step takes about 5 min...\n")
     utils = importr("utils")
     packnames = ("igraph", "reticulate","mvtnorm")
