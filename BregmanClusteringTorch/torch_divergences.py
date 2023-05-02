723851e72d31388cb11ef97726d76ce636cd8bde
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-05-02 15:51:21 -0300

    ""

--- BregmanClusteringTorch/torch_divergences.py
+++ BregmanClusteringTorch/torch_divergences.py
@@ -17,6 +17,7 @@ warnings.filterwarnings("ignore")
 
 #Bernoulli
 def logistic_loss(X,M):
+    print(X,M)
     total = BCELoss(X,M,reduce=False)
     #total = torch.where( X == 0, -torch.log( 1-M ), torch.log(X/M) )
     return total
