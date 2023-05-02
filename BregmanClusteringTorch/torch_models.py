05e0ef7ce84252140039607864f2f5068467837d
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-05-02 12:37:11 -0300

    ""

--- BregmanClusteringTorch/torch_models.py
+++ BregmanClusteringTorch/torch_models.py
@@ -272,7 +272,7 @@ class BregmanEdgeClusteringTorch( BaseEstimator, ClusterMixin ):
                                                 )
                                         )
             #print(L.shape,att_div.shape,graph_div.shape,edge_div.shape)
-            L[ q ] = att_div + 0.5*graph_div + edge_div
+            L[ q ] = att_div + graph_div + edge_div
         return torch.argmin( L )
     
     def predict(self, X, Y):
@@ -497,7 +497,7 @@ class BregmanEdgeClusteringTorchSparse( BaseEstimator, ClusterMixin ):
                                                 )
                                         )
             #print(L.shape,att_div.shape,graph_div.shape,edge_div.shape)
-            L[ q ] = att_div + 0.5*graph_div + edge_div
+            L[ q ] = att_div + graph_div + edge_div
         return torch.argmin( L )
     
     def predict(self, X, Y):
