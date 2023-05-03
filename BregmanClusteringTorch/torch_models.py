455be0822506bcec0bf45efea9a212a575b8bcf0
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-05-03 19:42:37 -0300

    ""

--- BregmanClusteringTorch/torch_models.py
+++ BregmanClusteringTorch/torch_models.py
@@ -520,19 +520,23 @@ class BregmanEdgeClusteringTorchSparse( BaseEstimator, ClusterMixin ):
                                         )
             """
             #print(len(v_indices_out),len(edge_indices_out),len(v_indices_in),len(edge_indices_in))
-            edge_div = self.reduce_by( self.edge_divergence(
-                                                X[edge_indices_out,:],
-                                                E[q,z_t[v_indices_out],:]
-                                                )
+            try:
+                edge_div = self.reduce_by( self.edge_divergence(
+                                                    X[edge_indices_out,:],
+                                                    E[q,z_t[v_indices_out],:]
+                                                    )
 
-                                        + 
+                                            + 
 
-                                        self.edge_divergence(
-                                                X[edge_indices_in,:],
-                                                E[z_t[v_indices_in],q,:]
-                                            ) 
+                                            self.edge_divergence(
+                                                    X[edge_indices_in,:],
+                                                    E[z_t[v_indices_in],q,:]
+                                                ) 
 
-                                        )
+                                            )
+            except:
+                print(len(v_indices_out),len(edge_indices_out),len(v_indices_in),len(edge_indices_in))
+
             #print(L.shape,att_div.shape,graph_div.shape,edge_div.shape)
             L[ q ] = att_div + 0.5*graph_div + edge_div
         return torch.argmin( L )
