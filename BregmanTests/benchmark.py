7c809a9433dde2e611ec0e9ebf5c5b3cdba8e110
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-05-20 22:47:13 -0300

    ""

--- BregmanTests/benchmark.py
+++ BregmanTests/benchmark.py
@@ -397,7 +397,7 @@ class BregmanBenchmark():
                 aris_both.append( adjusted_rand_score( z_true, z_pred_both ) )
                 aris_both_mean.append( np.mean( aris_both ) )
                 aris_both_std.append( np.std( aris_both ) )
-                print(X.shape)
+                print(X.sum())
                 X = Y = z_true = G = None
             stats["a"].append(a)
             stats["r"].append(r)
