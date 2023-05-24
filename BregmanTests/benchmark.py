6a216352f660395e04319af9479dbb3123d245ce
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-05-24 04:12:45 -0300

    ""

--- BregmanTests/benchmark.py
+++ BregmanTests/benchmark.py
@@ -2,6 +2,7 @@ import numpy as np
 import networkx as nx
 from BregmanTests.distributions import *
 from BregmanClustering.models import *
+from BregmanClustering.models import BregmanNodeEdgeAttributeGraphClustering as edgeBreg
 from BregmanClusteringTorch.torch_models import BregmanEdgeClusteringTorchSparse as sparseBreg
 from sklearn.metrics import adjusted_rand_score, normalized_mutual_info_score, accuracy_score
 from torch_geometric.utils import to_networkx,to_dense_adj,from_networkx
