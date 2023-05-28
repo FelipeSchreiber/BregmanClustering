5727b0901f3648160a7e3a3ee792b093f7fc7cf6
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-05-28 12:43:35 -0300

    ""

--- BregmanInitializer/init_cluster.py
+++ BregmanInitializer/init_cluster.py
@@ -155,9 +155,9 @@ class BregmanGraphClustering( BaseEstimator, ClusterMixin ):
             sum_j div_edge(e_ij, E[q,l,:])  
             """
             print(A[node,:].shape,M_out.shape)
-            edge_div = self.edge_divergence( A[node,:].flatten, M_out ).sum() \
-                        + self.edge_divergence( A[:,node].flatten, M_in ).sum()
-                        # - 2*self.edge_divergence(A[node,node],M_in[q])
+            edge_div = self.edge_divergence( A[node,:].flatten(), M_out ).sum() \
+                        + self.edge_divergence( A[:,node].flatten(), M_in ).sum()\
+                        - 2*self.edge_divergence(A[node,node],M_in[q])
             weight_div = 0
             if len(v_indices_out) > 0:
                 weight_div += np.sum( paired_distances(X[edge_indices_out,:],\
