94b983cde4a23bd6ecfe2309d62da2c07fde0aa0
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-06-13 20:06:08 -0300

    first commit spark

---
+++ BregmanClusteringSpark/src/main/scala/SimpleApp.scala
@@ -0,0 +1,13 @@
+import org.apache.spark.sql.SparkSession
+
+object SimpleApp {
+  def main(args: Array[String]) {
+    val logFile = "/home/felipe/spark-3.4.0-bin-hadoop3/README.md" // Should be some file on your system
+    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    spark.stop()
+  }
+}
