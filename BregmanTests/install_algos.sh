cc808b2e92746422394a7716fac95b6465529acf
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-06-24 17:44:28 -0300

    ""

--- BregmanTests/install_algos.sh
+++ BregmanTests/install_algos.sh
@@ -5,6 +5,36 @@ rm -rf ./data
 git clone https://github.com/glmbraun/CSBM/
 git clone https://github.com/stanleyn/AttributedSBM.git
 git clone https://github.com/bkamins/ABCDGraphGenerator.jl.git
+
+## INSTALL JULIA
+set -e
+
+#---------------------------------------------------#
+JULIA_VERSION="1.8.5" # any version ≥ 0.7.0
+JULIA_PACKAGES="IJulia BenchmarkTools PyCall PyPlot"
+JULIA_PACKAGES_IF_GPU="CUDA" # or CuArrays for older Julia versions
+JULIA_NUM_THREADS=4
+#---------------------------------------------------#
+
+if [ -z `which julia` ]; then
+  # Install Julia
+  JULIA_VER=`cut -d '.' -f -2 <<< "$JULIA_VERSION"`
+  echo "Installing Julia $JULIA_VERSION on the current Colab Runtime..."
+  BASE_URL="https://julialang-s3.julialang.org/bin/linux/x64"
+  URL="$BASE_URL/$JULIA_VER/julia-$JULIA_VERSION-linux-x86_64.tar.gz"
+  wget -nv $URL -O /tmp/julia.tar.gz # -nv means "not verbose"
+  tar -x -f /tmp/julia.tar.gz -C /usr/local --strip-components 1
+  rm /tmp/julia.tar.gz
+
+  # Install Packages
+  nvidia-smi -L &> /dev/null && export GPU=1 || export GPU=0
+  if [ $GPU -eq 1 ]; then
+    JULIA_PACKAGES="$JULIA_PACKAGES $JULIA_PACKAGES_IF_GPU"
+  fi
+  for PKG in `echo $JULIA_PACKAGES`; do
+    echo "Installing Julia package $PKG..."
+    julia -e 'using Pkg; pkg"add '$PKG'; precompile;"' &> /dev/null
+  done
+fi
 cd ./ABCDGraphGenerator.jl/utils/
-ls .
 julia install.jl
