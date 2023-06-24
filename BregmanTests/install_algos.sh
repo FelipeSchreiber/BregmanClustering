3823bb3b09ed6be77452bac993bfc974a1abc5d5
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-06-24 17:19:51 -0300

    ""

--- BregmanTests/install_algos.sh
+++ BregmanTests/install_algos.sh
@@ -5,3 +5,5 @@ rm -rf ./data
 git clone https://github.com/glmbraun/CSBM/
 git clone https://github.com/stanleyn/AttributedSBM.git
 git clone https://github.com/bkamins/ABCDGraphGenerator.jl.git
+cd ./ABCDGraphGenerator.jl/utils/
+julia install.jl
