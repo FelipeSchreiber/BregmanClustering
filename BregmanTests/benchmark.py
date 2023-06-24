96a7e0c2bf41f8d9323431df3a5f8b2711d9187e
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-06-24 17:15:16 -0300

    ""

--- BregmanTests/benchmark.py
+++ BregmanTests/benchmark.py
@@ -194,6 +194,7 @@ nout = "100"                  # number of vertices in graph that are outliers; o
         #cwd = os.getcwd()
         jl_path = find_jl()
         subprocess.call([f"{jl_path}",f"{path_to_ABCD_sampler}","my_config.toml"])
+        print(f"{jl_path}",f"{path_to_ABCD_sampler}","my_config.toml")
         X = np.array(pd.read_csv('deg.dat',header=None)[0])
         return X
     
