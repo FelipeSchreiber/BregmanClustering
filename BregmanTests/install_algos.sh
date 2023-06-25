c03ac0a6cd6e72fc834c10bc80877843ada52eda
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-06-25 00:08:51 -0300

    ""

--- BregmanTests/install_algos.sh
+++ BregmanTests/install_algos.sh
@@ -5,10 +5,21 @@ rm -rf ./data
 git clone https://github.com/glmbraun/CSBM/
 git clone https://github.com/stanleyn/AttributedSBM.git
 git clone https://github.com/bkamins/ABCDGraphGenerator.jl.git
-pwd=$(pwd)
-cd /
-wget https://julialang-s3.julialang.org/bin/linux/x64/1.8/julia-1.8.1-linux-x86_64.tar.gz
-tar zxvf julia-1.8.1-linux-x86_64.tar.gz -C /usr/local --strip-components 1
-cd $pwd 
+# wget https://julialang-s3.julialang.org/bin/linux/x64/1.8/julia-1.8.1-linux-x86_64.tar.gz
+# tar zxvf julia-1.8.1-linux-x86_64.tar.gz -C /usr/local --strip-components 1
+#---------------------------------------------------#
+JULIA_VERSION="1.8.5" # any version ≥ 0.7.0
+JULIA_PACKAGES="IJulia BenchmarkTools PyCall PyPlot"
+JULIA_PACKAGES_IF_GPU="CUDA" # or CuArrays for older Julia versions
+JULIA_NUM_THREADS=4
+
+# Install Julia
+JULIA_VER=`cut -d '.' -f -2 <<< "$JULIA_VERSION"`
+echo "Installing Julia $JULIA_VERSION on the current Colab Runtime..."
+BASE_URL="https://julialang-s3.julialang.org/bin/linux/x64"
+URL="$BASE_URL/$JULIA_VER/julia-$JULIA_VERSION-linux-x86_64.tar.gz"
+wget -nv $URL -O /tmp/julia.tar.gz # -nv means "not verbose"
+tar -x -f /tmp/julia.tar.gz -C /usr/local --strip-components 1
+rm /tmp/julia.tar.gz
 cd ./ABCDGraphGenerator.jl/utils/
 julia install.jl
