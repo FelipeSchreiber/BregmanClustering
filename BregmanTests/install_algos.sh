aa6e21c77dc8efbca56ca3f9f50c55a8cb663c04
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-06-25 00:33:36 -0300

    ""

--- BregmanTests/install_algos.sh
+++ BregmanTests/install_algos.sh
@@ -8,19 +8,21 @@ git clone https://github.com/bkamins/ABCDGraphGenerator.jl.git
 # wget https://julialang-s3.julialang.org/bin/linux/x64/1.8/julia-1.8.1-linux-x86_64.tar.gz
 # tar zxvf julia-1.8.1-linux-x86_64.tar.gz -C /usr/local --strip-components 1
 #---------------------------------------------------#
+set -e
 JULIA_VERSION="1.8.5" # any version ≥ 0.7.0
 JULIA_PACKAGES="IJulia BenchmarkTools PyCall PyPlot"
 JULIA_PACKAGES_IF_GPU="CUDA" # or CuArrays for older Julia versions
 JULIA_NUM_THREADS=4
-
-# Install Julia
-JULIA_VER=`cut -d '.' -f -2 <<< "$JULIA_VERSION"`
-echo "Installing Julia $JULIA_VERSION on the current Colab Runtime..."
-BASE_URL="https://julialang-s3.julialang.org/bin/linux/x64"
-URL="$BASE_URL/$JULIA_VER/julia-$JULIA_VERSION-linux-x86_64.tar.gz"
-URL="https://julialang-s3.julialang.org/bin/linux/x64/1.8/julia-1.8.1-linux-x86_64.tar.gz"
-wget -nv $URL -O /tmp/julia.tar.gz # -nv means "not verbose"
-tar -x -f /tmp/julia.tar.gz -C /usr/local --strip-components 1
-rm /tmp/julia.tar.gz
+if [ -z `which julia` ]; then
+  # Install Julia
+  JULIA_VER=`cut -d '.' -f -2 <<< "$JULIA_VERSION"`
+  echo "Installing Julia $JULIA_VERSION on the current Colab Runtime..."
+  BASE_URL="https://julialang-s3.julialang.org/bin/linux/x64"
+  URL="$BASE_URL/$JULIA_VER/julia-$JULIA_VERSION-linux-x86_64.tar.gz"
+  URL="https://julialang-s3.julialang.org/bin/linux/x64/1.8/julia-1.8.1-linux-x86_64.tar.gz"
+  wget -nv $URL -O /tmp/julia.tar.gz # -nv means "not verbose"
+  tar -x -f /tmp/julia.tar.gz -C /usr/local --strip-components 1
+  rm /tmp/julia.tar.gz
+fi
 cd ./ABCDGraphGenerator.jl/utils/
 julia install.jl
