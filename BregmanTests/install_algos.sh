02d03c99ca86f3e566567325cb51c40da1972f6e
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-06-25 00:24:47 -0300

    ""

--- BregmanTests/install_algos.sh
+++ BregmanTests/install_algos.sh
@@ -18,7 +18,7 @@ JULIA_VER=`cut -d '.' -f -2 <<< "$JULIA_VERSION"`
 echo "Installing Julia $JULIA_VERSION on the current Colab Runtime..."
 BASE_URL="https://julialang-s3.julialang.org/bin/linux/x64"
 URL="$BASE_URL/$JULIA_VER/julia-$JULIA_VERSION-linux-x86_64.tar.gz"
-URL="https://storage.googleapis.com/julialang2/bin/linux/x64/1.8/julia-1.8.1-linux-x86_64.tar.gz"
+URL="https://julialang-s3.julialang.org/bin/linux/x64/1.8/julia-1.8.1-linux-x86_64.tar.gz"
 wget -nv $URL -O /tmp/julia.tar.gz # -nv means "not verbose"
 tar -x -f /tmp/julia.tar.gz -C /usr/local --strip-components 1
 rm /tmp/julia.tar.gz
