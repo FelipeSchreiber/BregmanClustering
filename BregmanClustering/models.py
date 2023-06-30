98dd8d4725afc0661c31b8dd7ed687e5b260cce4
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-06-30 15:58:25 -0300

    replace constant by min val

--- BregmanClustering/models.py
+++ BregmanClustering/models.py
@@ -1328,7 +1328,7 @@ class BregmanNodeEdgeAttributeGraphClusteringSoft( BaseEstimator, ClusterMixin )
         H = pairwise_distances(Y,self.attribute_means,metric=self.attribute_divergence)
         for node in range(self.N):
             Ztilde[node,:] = self.computeTotalDiv(node,X,self.predicted_memberships,H)
-        c = Ztilde.max(axis=1)
+        c = Ztilde.min(axis=1)
         Ztilde -= c[:,None]
         Ztilde = self.communities_weights.reshape(1, -1)*np.exp(-Ztilde)
         return normalize(Ztilde, axis=1, norm='l1')
