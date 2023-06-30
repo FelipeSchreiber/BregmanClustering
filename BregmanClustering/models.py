75680f118e9a29514cb593618290f9b0fa3dcab9
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-06-30 18:01:09 -0300

    ""

--- BregmanClustering/models.py
+++ BregmanClustering/models.py
@@ -1332,7 +1332,7 @@ class BregmanNodeEdgeAttributeGraphClusteringSoft( BaseEstimator, ClusterMixin )
         H = pairwise_distances(Y,self.attribute_means,metric=self.attribute_divergence)
         for node in range(self.N):
             Ztilde[node,:] = self.computeTotalDiv(node,X,self.predicted_memberships,H)
-        c = Ztilde.max(axis=1)
+        c = Ztilde.min(axis=1)
         Ztilde -= c[:,None]
         soft_assign = self.communities_weights.reshape(1, -1)*np.exp(-Ztilde)
         if(np.isnan(soft_assign).any()):
