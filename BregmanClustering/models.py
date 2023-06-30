ee2ff9fbaf6064d3850c5993233f16e5c53b7c85
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-06-30 17:46:00 -0300

    max -> min

--- BregmanClustering/models.py
+++ BregmanClustering/models.py
@@ -1332,10 +1332,10 @@ class BregmanNodeEdgeAttributeGraphClusteringSoft( BaseEstimator, ClusterMixin )
         H = pairwise_distances(Y,self.attribute_means,metric=self.attribute_divergence)
         for node in range(self.N):
             Ztilde[node,:] = self.computeTotalDiv(node,X,self.predicted_memberships,H)
-        c = Ztilde.max(axis=1)
+        c = Ztilde.min(axis=1)
         Ztilde -= c[:,None]
         Ztilde = self.communities_weights.reshape(1, -1)*np.exp(-Ztilde)
-        Ztilde = np.nan_to_num(Ztilde)        
+        # Ztilde = np.nan_to_num(Ztilde)        
         return normalize(Ztilde, axis=1, norm='l1')
             
     def M_projection(self,X_,Y,Z):
@@ -1360,7 +1360,7 @@ class BregmanNodeEdgeAttributeGraphClusteringSoft( BaseEstimator, ClusterMixin )
         log_prob_total = 0
         for node in range(self.N):
             divs = self.computeTotalDiv(node,X_,self.predicted_memberships,H)
-            c = divs.max()
+            c = divs.min()
             divs -= c
             prob_i = self.communities_weights.dot(np.exp(-divs))
             log_prob_total += np.log(prob_i)
