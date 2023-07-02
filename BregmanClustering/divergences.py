fe65dcb88131a21c7f381b93917f55e70d796eb9
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-07-02 17:47:37 -0300

    ""

--- BregmanClustering/divergences.py
+++ BregmanClustering/divergences.py
@@ -27,6 +27,7 @@ SEE:
 def logistic_loss(X,M):
     # total = np.where( X == 0, -np.log( 1-M ), -np.log(M) )
     total = np.log(1 + np.exp(- (2*X - 1) * ( np.log(M/(1-M)) ) ))
+    total = np.clip(total,a_min=-1e6,a_max=1e6)
     return total.sum()
 
 #Multinomial | KL-divergence
