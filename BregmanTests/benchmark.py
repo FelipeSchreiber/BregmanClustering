787f51b5d3b3373a4d31805a3114972bd80f5558
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-07-07 12:45:10 -0300

    added attributes to benchmark

--- BregmanTests/benchmark.py
+++ BregmanTests/benchmark.py
@@ -200,6 +200,21 @@ nout = "100"                  # number of vertices in graph that are outliers; o
         G = nx.parse_edgelist(edges, nodetype=int)
         edges.close()
         com_df = pd.read_csv('com.dat',delimiter="\t",header=None)
+        counts = com_df[1].value_counts()
+        com_sizes = [counts[i] for i in range(com_df[1].max())]
+        self.communities_sizes = com_sizes
+        mapping = {}
+        sorted = com_df.sort_values(by=1)
+        # sorted[2] = np.arange(df.shape[0])
+        for i,node in enumerate(sorted[0]):
+            mapping[node] = i
+        G  = nx.relabel_nodes(G, mapping)
+        Y = self.generate_attributes()
+        labels_true = sorted[1].to_numpy()
+        if self.return_G:
+            for i in range(np.sum(self.communities_sizes)):
+                G.nodes[i]["x"] = Y[i,:].tolist()
+            return Y,labels_true,G
         return G,com_df
     
     def to_pyg_data(self,X,Y):
