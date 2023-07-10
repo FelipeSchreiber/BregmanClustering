fce958b452dad4ee957e73aebfcd882015f5ddc2
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-07-10 11:45:34 -0300

    ""

--- BregmanTests/benchmark.py
+++ BregmanTests/benchmark.py
@@ -1065,7 +1065,7 @@ nout = "100"                  # number of vertices in graph that are outliers; o
             
         # G_nx = to_networkx(data)
         # nx.set_edge_attributes(G_nx,E,"weight")
-        preds = fit_leiden(np.nonzero(A),E,Y.shape[0])
+        leiden_labels_ = fit_leiden(np.nonzero(A),E,Y.shape[0])
         # G_nx = nx.from_numpy_array(A)
         # G = ig.Graph.from_networkx(G_nx)
         # partition = la.find_partition(G, la.ModularityVertexPartition)
@@ -1076,7 +1076,7 @@ nout = "100"                  # number of vertices in graph that are outliers; o
                 model_hard.memberships_from_graph,
                 model_hard.memberships_from_attributes,
                 kmeans.labels_,
-                np.array(partition.membership),
+                leiden_labels_.flatten(),
                 SC.labels_,
                 SC2.labels_,
                 SC3.labels_
