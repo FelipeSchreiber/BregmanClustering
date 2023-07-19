b06c2437817b8a1df6ea7d3d80c8d7edfec5a6b1
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-07-19 19:33:28 -0300

    ""

--- BregmanClustering/models.py
+++ BregmanClustering/models.py
@@ -952,6 +952,7 @@ class BregmanNodeEdgeAttributeGraphClusteringEfficient( BaseEstimator, ClusterMi
         return U
     
     def computeAttributeMeans( self, Y, Z ):
+        print(Y.shape,Z.shape)
         attribute_means = np.dot(Z.T, Y)/(Z.sum(axis=0) + 10 * np.finfo(Z.dtype).eps)[:, np.newaxis]
         return attribute_means
     
