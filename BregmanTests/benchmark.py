bcb9e0c6e434c805951e9c92a762378cc19c6176
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-08-17 00:22:04 -0300

    trying raw in kernel init

--- BregmanTests/benchmark.py
+++ BregmanTests/benchmark.py
@@ -1071,7 +1071,7 @@ nout = "100"                  # number of vertices in graph that are outliers; o
         # SC4.fit(A,E,Y)
 
         SC5 = BregmanKernelClustering(K, 
-                edgeSimilarity = "gaussian",
+                edgeSimilarity = "raw",
                 weightDistribution = "gaussian",
                 attributeDistribution = "gaussian",
                 single_metric=False,
